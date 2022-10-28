3aa7cc6b5b01c997c135d7d65daa516a73067c99
Ali Cemilcan Ciftarslan <ciftarsl@sheridancollege.ca>
2022-10-27 23:51:18 -0400

    cors

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import psycopg2
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from psycopg2.extras import RealDictCursor
 
 from config import Settings
 from database import engine
 from repo import models
+from repo.models import Base
 from routers import auth, post, user, vote
 
 # get a session from the database and close it when request is done
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
